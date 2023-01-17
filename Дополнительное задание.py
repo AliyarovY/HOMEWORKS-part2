e3450e1678e9ee19a3817ed3fde3d3203ba4fa20
-- <yaliyarov@mail.ru>
2023-01-17 17:45:00 +0300

    14

--- Дополнительное задание.py
+++ Дополнительное задание.py
@@ -7,9 +7,11 @@ for _ in iter(bool, 111):
             sock.bind(('127.0.0.1', 2000))
             sock.listen(0)
             cl, _ = sock.accept()
+            data = cl.recv(1024)
             hdrs = b'HTTP/1.1 200 OK\r\nContent-Type: text/html; chrset=utf-8\r\n\r\n'
-            cl.send(hdrs + 'Hello, World wide web!'.encode('utf-8'))
+            cl.send(hdrs + data)
         except:
             continue
         finally:
             cl.shutdown(socket.SHUT_WR)
+
