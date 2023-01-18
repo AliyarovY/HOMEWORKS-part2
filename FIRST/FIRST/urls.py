536657e1bb5f389a524ea000f2925d07d1dc127a
-- <yaliyarov@mail.ru>
2023-01-18 05:54:42 +0300

    15

--- FIRST/FIRST/urls.py
+++ FIRST/FIRST/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('feedbck', include('feedbck.urls')),
     path('admin/', admin.site.urls),
     path('catalog', include('catalog.urls')),
     path('', include('home.urls'))
